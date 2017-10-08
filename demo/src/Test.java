382e935c2f23b8f3b94bb45be8e472fcbb519a05
xnx3 <mail@xnx3.com>
2017-10-08 19:06:57 +0800

    xnx3
    import

---
+++ demo/src/Test.java
@@ -0,0 +1,8 @@
+import com.xnx3.Language;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println(Language.show("chinese", "login"));
+		System.out.println(Language.show("english", "login"));
+	}
+}
